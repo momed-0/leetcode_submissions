1d89b5c3502ed9d1a4867bb29ff4bf7e8afde492
Mohammed <90509585+momed-0@users.noreply.github.com>
2025-12-26 18:08:34 +0530

    Add getConcatenation function
    Implement function to concatenate an array with itself.

--- arrays_and_hashing/easy/concatenation-of-array/func.go
+++ arrays_and_hashing/easy/concatenation-of-array/func.go
@@ -1,1 +1,19 @@
+// https://leetcode.com/problems/concatenation-of-array/
+func getConcatenation(nums []int) []int {
+    length := len(nums);
+    ans := make([]int, 2 *length);
+    for i := range length {
+        ans[i] = nums[i];
+        ans[i + length] = nums[i];
+    }
+    return ans;
+}
 
+/*
+
+nums[]
+ans = []
+for i in nums
+    ans[i] = nums[i]
+    ans[i+nums.length] = nums[i]
+*/
