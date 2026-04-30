4890370b6899fbb6e5239e59e88496d72e8bf3a6
Mohammed <90509585+momed-0@users.noreply.github.com>
2026-05-01 01:15:21 +0530

    Add longest palindromic substring solution
    Implement a solution to find the longest palindromic substring using dynamic programming.

---
+++ dynamic_programming/1d/medium/longest-palindromic-substring-work-in-progress/main.cpp
@@ -0,0 +1,54 @@
+// Given a string s, return the longest in s.
+
+ 
+
+// Example 1:
+
+// Input: s = "babad"
+// Output: "bab"
+// Explanation: "aba" is also a valid answer.
+
+// Example 2:
+
+// Input: s = "cbbd"
+// Output: "bb"
+
+/*
+THe idea is iterate through every possible substring and check if palindrom, if yes store the result to remeber the computation
+  */
+
+// i would say very stupid and inefficient
+// TC - O(N2) SC - O(2N)
+class Solution {
+public:
+    int find_length_palindrome(int i, int j, string& s ){
+        if (i>j) return 0;
+        int leftptr =i; int rightptr = j;
+        while (leftptr <= rightptr) {
+            if (s[leftptr] != s[rightptr]) return 0;
+            leftptr++; rightptr--;
+        }
+        return (j-i)+1;
+    }
+    string longestPalindrome(string s) {
+        
+        vector<vector<int>> dp(s.size(), vector<int> (s.size(), -1));
+        int maxi = 0; int max_i = 0; int max_j = 0;
+
+        for(int i =0; i< s.size(); i++) {
+            for(int j=s.size()-1; j >=i; j--) {
+
+                if(dp[i][j] < 0) {
+                    dp[i][j] = find_length_palindrome(i, j, s);
+                } 
+                int length_of_palindrome = dp[i][j];
+                
+                if (length_of_palindrome > maxi) {
+                     maxi = length_of_palindrome;
+                     max_i = i; max_j = j;
+                }
+            }
+        }
+        return s.substr(max_i, max_j - max_i + 1);
+    }
+};
