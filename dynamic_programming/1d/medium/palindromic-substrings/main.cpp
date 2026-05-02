ab549922de4af01d5f41399f711e1050393a3fc4
Mohammed <90509585+momed-0@users.noreply.github.com>
2026-05-02 23:58:47 +0530

    Implement palindromic substring counter
    Added a class for counting palindromic substrings using dynamic programming.

--- dynamic_programming/1d/medium/palindromic-substrings/main.cpp
+++ dynamic_programming/1d/medium/palindromic-substrings/main.cpp
@@ -48,3 +48,30 @@ public:
         return n + count;
     }
 };
+
+
+// dp inefficient solution
+class Solution {
+public:
+    bool isPal(string& s,int i,int j) {
+        while(i<j) {
+            if(s[i] != s[j]) return false;
+            ++i;--j;
+        }
+        return true;
+    }
+    int countSubstrings(string s) {
+        int count = 0;
+        vector<vector<int>> dp(s.size(), vector<int> (s.size(), -1)) ;
+        for(int i=0;i<s.size();++i) {
+            for(int j=i;j<s.size();++j) {
+                if(dp[i][j] < 0) {
+                    dp[i][j] = isPal(s, i, j);
+                }
+            
+                if(dp[i][j]) ++count;
+            }
+        }
+        return count;
+    }
+};
