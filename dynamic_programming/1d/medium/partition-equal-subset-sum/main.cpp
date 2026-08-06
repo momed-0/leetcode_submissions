2d4713e2a11fc8b355d28d0f25440decd31e5653
Mohammed <90509585+momed-0@users.noreply.github.com>
2026-08-06 15:47:19 +0530

    Add 2D DP solution for partition equal subset sum
    Implemented a 2D dynamic programming solution for the partition equal subset sum problem with memoization. The solution includes a recursive function to check if a subset with a target sum can be formed.

--- dynamic_programming/1d/medium/partition-equal-subset-sum/main.cpp
+++ dynamic_programming/1d/medium/partition-equal-subset-sum/main.cpp
@@ -44,3 +44,37 @@ public:
         return solve(nums, 0, total_sum/2); // find a subset with sum equal to total_sum/2;
     }
 };
+
+// 2D Dp solution with O(N*target) TC and O(target) sc Memoization
+class Solution {
+public:
+    // O(N * target) COmplexity, SC - O(target)
+    int solve(vector<int>& nums, int ind, int target, vector<vector<int>>& dp) {
+        if (target == 0) return 1; // achieved the target
+        if (target < 0 || ind  >= nums.size()) return 0; // out of boundary
+        if (dp[ind][target] >= 0) return dp[ind][target];
+        // two cases, either take current index and recurse or don't take
+        if (solve(nums, ind+1, target - nums[ind], dp) || solve(nums, ind+1, target, dp)) {
+            return dp[ind ][target] = 1;
+        }
+        return dp[ind][target] = 0; // not possible
+    }
+    bool canPartition(vector<int>& nums) {
+        int total_sum = 0;
+        for(int i=0; i< nums.size(); i++) {
+            total_sum += nums[i];
+        }
+        if (total_sum  % 2 != 0) {
+            // sum is odd, no way to partition
+            return false;
+        }
+         // three states required for dp, -1 -> not computed, 0 ->false, 1 -> true
+        vector<vector<int>> dp (nums.size(), vector<int> ((total_sum/2) + 1, -1));
+        if (solve(nums, 0, total_sum/2, dp) == 1) {
+            return true;
+        }
+        return false;
+    }
+};
+
+
