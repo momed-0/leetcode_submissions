ec4a2a1b30f45786b5d943b2810949fca319cdf2
Mohammed <90509585+momed-0@users.noreply.github.com>
2026-09-09 09:50:30 +0530

    Optimize stoneGameIII function for space complexity
    Refactored stoneGameIII function to optimize space complexity using three variables instead of a dp array.

--- dynamic_programming/1d/hard/stone-game-iii/main.cpp
+++ dynamic_programming/1d/hard/stone-game-iii/main.cpp
@@ -88,5 +88,92 @@ public:
     }
 };
 
+class Solution {
+public:
+ //O(N) both TC and SC
+    // advantage that the current player has it at ind 
+    // each t=player have to maximise the advantage
+    int solve(vector<int>& nums, int n, int ind, vector<int>& dp) {
+       if (ind >= n) return 0; // boundary case
+       if (dp[ind] >= 0) return dp[ind];
+       int take1 = INT_MIN, take2 = INT_MIN, take3 = INT_MIN;
+       
+       if (ind  + 2 < n) {
+            take3 = (nums[ind] + nums[ind+1] + nums[ind+2]) - solve(nums, n, ind+3, dp);
+       }
+       if (ind + 1 < n) {
+            take2 = (nums[ind] + nums[ind+1]) - solve(nums, n ,ind+2, dp);
+       }
+       take1 = nums[ind] - solve(nums, n, ind+1,dp);
+       return dp[ind] = max(take1, max(take3, take2)); // each player maximisng advantage 
+    }
+    string stoneGameIII(vector<int>& stoneValue) {
+        vector<int> dp(stoneValue.size() +1 , -1);
+        int advantage_of_alice = solve(stoneValue, stoneValue.size(), 0, dp);
+        // basically Solve(0) = alice's advantage - BObs advantage 
+        if (advantage_of_alice == 0) return "Tie";
+        else if (advantage_of_alice > 0) return  "Alice";
+        else return  "Bob";
+    }
+};
 
+class Solution {
+public:
+ //O(N) both TC and SC
+    // advantage that the current player has it at ind 
+    // each t=player have to maximise the advantage
+    string stoneGameIII(vector<int>& stoneValue) {
+        vector<int> dp(stoneValue.size() +1 , -1);
+        int n = stoneValue.size();
+        // start from the end
+        dp[n] = 0;
+        for (int i = n -1; i >= 0; i--) {
+            int take1 = INT_MIN, take2 = INT_MIN, take3 = INT_MIN;
+            if (i  + 2 < n) {
+                    take3 = (stoneValue[i] + stoneValue[i+1] + stoneValue[i+2]) - dp[i+3];
+            }
+            if (i + 1 < n) {
+                    take2 = (stoneValue[i] + stoneValue[i+1]) - dp[i+2];
+            }
+            take1 = stoneValue[i] - dp[i+1];
+            dp[i] = max(take1, max(take3, take2)); // each player maximisng advantage 
+        }
+
+        int advantage_of_alice = dp[0];
+        // basically Solve(0) = alice's advantage - BObs advantage 
+        if (advantage_of_alice == 0) return "Tie";
+        else if (advantage_of_alice > 0) return  "Alice";
+        else return  "Bob";
+    }
+};
  
+class Solution {
+public:
+// space optimized dp
+ //O(N) both TC
+    // advantage that the current player has it at ind 
+    // each t=player have to maximise the advantage
+    string stoneGameIII(vector<int>& stoneValue) {
+        // start from the end
+        int n = stoneValue.size();
+        int first = 0; int second = 0; int third = 0;
+        for (int i = n -1; i >= 0; i--) {
+            int take1 = INT_MIN, take2 = INT_MIN, take3 = INT_MIN;
+            if (i  + 2 < n) {
+                    take3 = (stoneValue[i] + stoneValue[i+1] + stoneValue[i+2]) - third;
+            }
+            if (i + 1 < n) {
+                    take2 = (stoneValue[i] + stoneValue[i+1]) - second;
+            }
+            take1 = stoneValue[i] - first;
+            third = second;
+            second = first;
+            first = max(take1, max(take3, take2)); // each player maximisng advantage 
+        }
+        int advantage_of_alice = first;
+        // basically Solve(0) = alice's advantage - BObs advantage 
+        if (advantage_of_alice == 0) return "Tie";
+        else if (advantage_of_alice > 0) return  "Alice";
+        else return  "Bob";
+    }
+};
