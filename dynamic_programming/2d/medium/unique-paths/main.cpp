2cd18203293470ed33d8e05ad1416d7264fc705a
Mohammed <90509585+momed-0@users.noreply.github.com>
2026-09-11 22:45:30 +0530

    Add unique paths solution using recursion and memoization
    Implemented a recursive solution with memoization for calculating unique paths in a grid.

---
+++ dynamic_programming/2d/medium/unique-paths/main.cpp
@@ -0,0 +1,43 @@
+// There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
+
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+// The test cases are generated so that the answer will be less than or equal to 2 * 109.
+class Solution {
+public:
+    // TC -> O(2^m+n) -> recursion depth
+    int find_ways(int i, int j, int m, int  n) {
+        if (i == m-1 && j == n-1) { // reached the finish, return 1
+            return 1;
+        }
+        if (i >= m || j >= n) {// out of boundary
+            return 0;
+        }
+        // go right and go down
+        return find_ways(i+1, j, m, n) + find_ways(i, j+1, m, n);
+    }
+    int uniquePaths(int m, int n) {
+        return find_ways(0, 0, m, n);
+    }
+};
+
+class Solution {
+public:
+    // TC -> O(m*n) 
+    // SC - O(m+n)
+    int find_ways(int i, int j, int m, int  n, vector<vector<int>>& dp) {
+        if (i == m-1 && j == n-1) { // reached the finish, return 1
+            return 1;
+        }
+        if (i >= m || j >= n) {// out of boundary
+            return 0;
+        }
+        if (dp[i][j] >= 0) return dp[i][j];
+        // go right and go down
+        return dp[i][j] = find_ways(i+1, j, m, n, dp) + find_ways(i, j+1, m, n, dp);
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m , vector<int> (n , -1));
+        return find_ways(0, 0, m, n, dp);
+    }
+};
