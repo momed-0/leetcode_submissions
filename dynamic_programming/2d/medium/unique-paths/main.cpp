6d74f5cb96eed3b4bec02ea7d0acfed9ae4da8f6
Mohammed <90509585+momed-0@users.noreply.github.com>
2026-09-14 11:45:55 +0530

    Implement bottom-up DP for unique paths problem

--- dynamic_programming/2d/medium/unique-paths/main.cpp
+++ dynamic_programming/2d/medium/unique-paths/main.cpp
@@ -41,3 +41,22 @@ public:
         return find_ways(0, 0, m, n, dp);
     }
 };
+
+// bottom up
+class Solution {
+public:
+    // TC -> O(m*n) 
+    // SC - O(m*n)
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m , vector<int> (n , 0));
+        dp[m-1][n-1] = 1;   
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1; j >= 0; j --) {
+                if (i == m-1 && j== n-1) continue;
+                if (i + 1 < m ) dp[i][j] += dp[i+1][j];
+                if (j + 1 < n ) dp[i][j] += dp[i][j+1];
+            }
+        }
+        return dp[0][0];
+    }
+};
